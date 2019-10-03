8831be5ff3e30fce697ba297fd4c018d8f6e72e2
Elie NEHME <elie29@gmail.com>
2019-10-03 14:00:13 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { RootComponent } from './components/root/root.component';
+
+@NgModule({
+  declarations: [RootComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [RootComponent]
+})
+export class AppModule {}
