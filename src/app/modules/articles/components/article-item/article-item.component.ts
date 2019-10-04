ae684165337d751e005a26df4d3a6ad57c69187d
Elie NEHME <elie29@gmail.com>
2019-10-04 12:11:21 +0200

    Animated article item

--- src/app/modules/articles/components/article-item/article-item.component.ts
+++ src/app/modules/articles/components/article-item/article-item.component.ts
@@ -7,4 +7,21 @@ import { Component, HostBinding } from '@angular/core';
 })
 export class ArticleItemComponent {
   @HostBinding('attr.class') css = 'row';
+
+  article = {
+    title: 'Angular',
+    meta: '(angular.io)',
+    href: '//angular.io',
+    points: 5
+  };
+
+  upvote(): false {
+    this.article.points++;
+    return false; // required when href exists on a tag
+  }
+
+  downvote(): false {
+    this.article.points--;
+    return false;
+  }
 }
