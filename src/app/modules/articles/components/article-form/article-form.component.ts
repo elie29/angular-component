efa1b42838e7631b62160a153c7dcfac32b1c14a
Elie NEHME <elie29@gmail.com>
2019-10-04 12:31:41 +0200

    Used view child

--- src/app/modules/articles/components/article-form/article-form.component.ts
+++ src/app/modules/articles/components/article-form/article-form.component.ts
@@ -1,16 +1,35 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-article-form',
   templateUrl: './article-form.component.html',
   styleUrls: ['./article-form.component.scss']
 })
-export class ArticleFormComponent {
+export class ArticleFormComponent implements OnInit {
   @Output() addedArticle = new EventEmitter<{ title: string; link: string }>();
+  @ViewChild('title', { static: true }) title: ElementRef;
+  @ViewChild('link', { static: false }) link: ElementRef;
+
+  ngOnInit(): void {
+    console.log('static true: title is available', this.title);
+    console.log(
+      'static false: link is available in ngAfterViewInit',
+      this.link
+    );
+  }
 
   addArticle(title: string, link: string): void {
     if (title && link) {
       this.addedArticle.emit({ title, link });
+      this.title.nativeElement.value = '';
+      this.link.nativeElement.value = '';
     }
   }
 }
