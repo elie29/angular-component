07334bd297b2c38cd309608de7ba0a07ad90654c
Elie NEHME <elie29@gmail.com>
2019-10-04 12:04:35 +0200

    Added ngSubmit

--- src/app/modules/articles/articles.module.ts
+++ src/app/modules/articles/articles.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { ArticleFormComponent } from './components/article-form/article-form.component';
 import { ArticleItemComponent } from './components/article-item/article-item.component';
@@ -7,7 +8,7 @@ import { ArticlesComponent } from './components/articles/articles.component';
 
 @NgModule({
   declarations: [ArticlesComponent, ArticleFormComponent, ArticleItemComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [ArticlesComponent]
 })
 export class ArticlesModule {}
